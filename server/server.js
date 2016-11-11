98862c132cf88ca2e18654ce4ef02167c147bedb
atakassamarrentie <juhakos87@gmail.com>
2016-11-11 10:01:23 +0100

    https

--- server/server.js
+++ server/server.js
@@ -2,26 +2,48 @@
 
 var loopback = require('loopback');
 var boot = require('loopback-boot');
-
-
-
+var https = require('https');
+var http = require('http');
+var sslConfig = require('./ssl-config');
+var httpsRedirect = require('./middleware/https-redirect');
+var path = require('path');
 var app = module.exports = loopback();
+var options = {
+  key: sslConfig.privateKey,
+  cert: sslConfig.certificate
+};
+
+
 app.use(loopback.token({ model: app.models.accessToken }));
 app.start = function () {
   // start the web server
-  return app.listen(function () {
-    app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
-    console.log('Web server listening at: %s', baseUrl);
-    if (app.get('loopback-component-explorer')) {
-      var explorerPath = app.get('loopback-component-explorer').mountPath;
-      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
-    }
-    /*var j = schedule.scheduleJob('* * * * * *', function () {
-      console.log('The answer to life, the universe, and everything!');
-    });*/
+  var host = app.get('host')
+  var port = app.get('port')
+  var server = https.createServer(options, app);
+  var baseUrl = `https://${host}:${port}`;
+  let httpsPort = app.get('https-port');
+  let redirectPort = app.get('redirect-port');
+  http.createServer(app).listen(redirectPort, function () {
+    console.log('Http for redirect listening on port ', redirectPort);
   });
+  app.use(httpsRedirect({
+    httpsPort: httpsPort
+  }));
+
+  server.listen(port, function () {
+    app.emit('started', baseUrl);
+    console.log('warn', `LoopBack server listening @ ${baseUrl}/`);
+    if (app.get('loopback-component-explorer')) {
+      let explorerPath = app.get('loopback-component-explorer').mountPath;
+      console.log('warn', `Browse your REST API @ ${baseUrl}${explorerPath}/`);
+    }
+  });
+  return server;
 };
+  /*var j = schedule.scheduleJob('* * * * * *', function () {
+    console.log('The answer to life, the universe, and everything!');
+  });*/
+
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
