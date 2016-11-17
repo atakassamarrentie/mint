176fcb32f40dfd1318f2f0e9bbff4bbca97a8228
atakassamarrentie <juhakos87@gmail.com>
2016-11-17 08:40:12 +0100

    .gitignore

--- client/bower_components/angular-trustpass/index.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-require('./src/tr-trustpass');
-module.exports = 'trTrustpass';
--- common/models/payment.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-var schedule = require('node-schedule');
-module.exports = function (Payment) {
-    /*var j = schedule.scheduleJob('* * * * * *', function () {
-        console.log('Done');
-    })*/
-}
